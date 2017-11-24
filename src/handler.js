2a4921a0c8b49127e806aca7c973056fc07c093a
Erica Windisch <erica@iopipe.com>
2017-11-24 19:18:02 +0000

    Restructure a bit, doc env vars
    Signed-off-by: Erica Windisch <erica@iopipe.com>

--- src/handler.js
+++ src/handler.js
@@ -2,30 +2,32 @@
 
    set environment variables:
   
-   - SLACK_VERIFICATION_TOKEN
-   - SLACK_ACCESS_TOKEN
+   - S3_BUCKET (required)
+   - SLACK_VERIFICATION_TOKEN (required)
+   - SLACK_ACCESS_TOKEN (required)
+   - MIN_CONFIDENCE (default 75)
+   - MAX_LABELS (default 10)
+
 */
-
-const https = require('https'),
-      qs = require('querystring'),
-      AWS = require('aws-sdk'),
-      request = require('request'),
-      VERIFICATION_TOKEN = process.env.SLACK_VERIFICATION_TOKEN,
-      ACCESS_TOKEN = process.env.SLACK_ACCESS_TOKEN,
-      MIN_CONFIDENCE = process.env.MIN_CONFIDENCE,
-      MAX_LABELS = process.env.MAX_LABELS;
-
-const s3 = new AWS.S3({apiVersion: '2006-03-01'}),
-      rekognition = new AWS.Rekognition({apiVersion: '2016-06-27'});
-
-const imgRegex = /(\S\.(png|jpeg|jpg|jpeg-large|jpg-large))(?:\s+|$)/ig
-
-
 const profilerPlugin = require('iopipe-plugin-profiler');
 const tracePlugin = require('iopipe-plugin-trace');
 const iopipe = require('iopipe')({
   plugins: [profilerPlugin(), tracePlugin()]
 });
+
+const https = require('https'),
+      qs = require('querystring'),
+      AWS = require('aws-sdk'),
+      request = require('request'),
+      s3 = new AWS.S3({apiVersion: '2006-03-01'}),
+      rekognition = new AWS.Rekognition({apiVersion: '2016-06-27'});
+
+const imgRegex = /(\S\.(png|jpeg|jpg|jpeg-large|jpg-large))(?:\s+|$)/ig,
+      VERIFICATION_TOKEN = process.env.SLACK_VERIFICATION_TOKEN,
+      ACCESS_TOKEN = process.env.SLACK_ACCESS_TOKEN,
+      S3_BUCKET = process.env.S3_BUCKET,
+      MIN_CONFIDENCE = process.env.MIN_CONFIDENCE || 75,
+      MAX_LABELS = process.env.MAX_LABELS || 10;
 
 exports.handler = iopipe((event, context) => {
   context.succeed('Wow!');
@@ -74,12 +76,12 @@ async function handleEvent(slackEvent, callback) {
     context.iopipe.mark.start('s3-putObject')
     s3.putObject({
       Body: imageData,
-      Bucket: process.env.S3_BUCKET,
+      Bucket: S3_BUCKET,
       Key: imgFilename 
     }, function s3putComplete(err, data) {
       context.iopipe.mark.stop('s3-putObject')
       resolve(err, {
-        Bucket: process.env.S3_BUCKET,
+        Bucket: S3_BUCKET,
         Name: imgFilename
       });
     });
